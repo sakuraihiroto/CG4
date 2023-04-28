75a2aae438745458edafd32e59a18a82513cff20
hirotosakurai <k021g1130@g.neec.ac.jp>
2023-04-28 10:23:04 +0900

    01-07

--- DirectXGame/3d/FbxLoader.cpp
+++ DirectXGame/3d/FbxLoader.cpp
@@ -1,49 +1,126 @@
 ﻿#include "FbxLoader.h"
 #include <cassert>
-
-
-const std::string FbxLoader::baseDirectory = "Resources/";
-
+using namespace DirectX;
+//01_07 P24
 FbxLoader* FbxLoader::GetInstance()
 {
     static FbxLoader instance;
     return &instance;
 }
+/// <summary>
+/// 静的メンバー変数の実体
+/// </summary>
+const std::string FbxLoader::baseDirectory =
+"Resources/";
 
 void FbxLoader::Initialize(ID3D12Device* device)
 {
+    //再初期化チェック
     assert(fbxManager == nullptr);
-
+    //引数からメンバ変数に代入
     this->device = device;
-
+    //FBXマネージャーの生成
     fbxManager = FbxManager::Create();
-
+    //FBXマネージャーの入出力設定
     FbxIOSettings* ios = FbxIOSettings::Create(fbxManager, IOSROOT);
     fbxManager->SetIOSettings(ios);
-
+    //FBXインポータの生成
     fbxImporter = FbxImporter::Create(fbxManager, "");
 }
 
 void FbxLoader::Finalize()
 {
+    //各種FBXインスタンスの破棄
     fbxImporter->Destroy();
     fbxManager->Destroy();
 }
 
 void FbxLoader::LoadModelFromFile(const string& modelName)
 {
-    const string directoryPath = baseDirectory + modelName + "/";
-
+    //モデルと同じ名前のフォルダから読み込み
+    const string directoryPath = baseDirectory +
+        modelName + "/";
+    //拡張子.fbxを附加
     const string fileName = modelName + ".fbx";
+    //連結してフルパスを得る
+    const string fullPath = directoryPath + fileName;
 
-    const string fullpath = directoryPath + fileName;
-
-    if (!fbxImporter->Initialize(fullpath.c_str(), -1, fbxManager->GetIOSettings()))
-    {
+    //ファイルを指定してFBXファイルを読み込む
+    if (!fbxImporter->Initialize(fullPath.c_str(),
+        -1, fbxManager->GetIOSettings())) {
         assert(0);
     }
+    //シーン生成
+    FbxScene* fbxScene =
+        FbxScene::Create(fbxManager, "fbxScene");
 
-    FbxScene* fbxScene = FbxScene::Create(fbxManager, "fbxScene");
-
+    //ファイルからロードしたFBXの情報をシーンにインポート
     fbxImporter->Import(fbxScene);
+
+    //モデル生成
+    Model* model = new Model();
+    model->name = modelName;
+    //FBXノードの数を取得
+    int nodeCount = fbxScene->GetNodeCount();
+    //あらかじめ必要数分のメモリを確保することで,アドレスがずれるのを予防
+    model->nodes.reserve(nodeCount);
+    //ルートノードから順に解析してモデルに流し込む
+    ParseNodeRecursive(model, fbxScene->GetRootNode());
+    //FBXシーン解放
+    fbxScene->Destroy();
+
+}
+
+void FbxLoader::ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent)
+{
+    //ノード名を取得
+    string name = fbxNode->GetName();
+    //モデルにノードを追加
+    model->nodes.emplace_back();
+    Node& node = model->nodes.back();
+    //ノード名を取得
+    node.name = fbxNode->GetName();
+
+    //FBXノードのローカル移動情報
+    FbxDouble3 rotation = fbxNode->LclRotation.Get();
+    FbxDouble3 scaling = fbxNode->LclScaling.Get();
+    FbxDouble3 translation = fbxNode->LclTranslation.Get();
+
+    //形式変換して代入
+    node.rotation = { (float)rotation[0],(float)rotation[1],(float)rotation[2],0.0f };
+    node.scaling = { (float)scaling[0],(float)scaling[1],(float)scaling[2],0.0f };
+    node.translation = { (float)translation[0],(float)translation[1],(float)translation[2],1.0f };
+
+    //回転角をDegree(度)からラジアンに変換
+    node.rotation.m128_f32[0] = XMConvertToRadians(node.rotation.m128_f32[0]);
+    node.rotation.m128_f32[1] = XMConvertToRadians(node.rotation.m128_f32[1]);
+    node.rotation.m128_f32[2] = XMConvertToRadians(node.rotation.m128_f32[2]);
+
+    //スケール,回転,平行移動行列の計算
+    XMMATRIX matScaling, matRotation, matTranslation;
+    matScaling = XMMatrixScalingFromVector(node.scaling);
+    matRotation = XMMatrixRotationRollPitchYawFromVector(node.rotation);
+    matTranslation = XMMatrixTranslationFromVector(node.translation);
+
+    //ローカル変形行列の計算
+    node.transform = XMMatrixIdentity();
+    node.transform *= matScaling;//ワールド行列にスケーリングを反映
+    node.transform *= matRotation;//ワールド行列に回転を反映
+    node.transform *= matTranslation;//ワールド行列に平行移動を反映
+
+    //グローバル変形行列の計算
+    node.globalTransform = node.transform;
+    if (parent)
+    {
+        node.parent = parent;
+        //親の変形を乗算
+        node.globalTransform *= parent->globalTransform;
+    }
+
+    //FBXノードのメッシュ情報を解析(Todo)
+
+    //子ノードに対して再帰呼び出し
+    for (int i = 0; i < fbxNode->GetChildCount(); i++) {
+        ParseNodeRecursive(model, fbxNode->GetChild(i), &node);
+    }
 }
--- DirectXGame/3d/FbxLoader.h
+++ DirectXGame/3d/FbxLoader.h
@@ -1,13 +1,21 @@
 ﻿#pragma once
 
 #include "fbxsdk.h"
-
+#include "Model.h"
 #include <d3d12.h>
 #include <d3dx12.h>
-#include <string>
+#include<string>
 
 class FbxLoader
 {
+private://エイリアス
+	//stdを省略
+	using string = std::string;
+
+public:
+	//モデル格納ルートパス
+	static const string baseDirectory;
+
 public:
 	/// <summary>
 	/// シングルトンインスタンスの取得
@@ -15,6 +23,28 @@ public:
 	/// <returns>インスタンス</returns>
 	static FbxLoader* GetInstance();
 
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="device">D3D12デバイス</param>
+	void Initialize(ID3D12Device* device);
+	/// <summary>
+	/// 後始末
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+	/// ファイルからFBXファイル読み込み
+	/// </summary>
+	/// <param name="modelName">モデル名</param>
+	void LoadModelFromFile(const string& modelName);
+	/// <summary>
+	/// 再帰的にノード構成を解析
+	/// </summary>
+	/// <param name="model">読み込み先モデルオブジェクト</param>
+	/// <param name="fbxNode">解析対象のノード</param>
+	void ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent = nullptr);
 private:
 	// privateなコンストラクタ（シングルトンパターン）
 	FbxLoader() = default;
@@ -25,28 +55,12 @@ private:
 	// コピー代入演算子を禁止（シングルトンパターン）
 	void operator=(const FbxLoader& obj) = delete;
 
-public:
-
-	void Initialize(ID3D12Device* device);
-
-	void Finalize();
-
 private:
-
+	//D3D12デバイス
 	ID3D12Device* device = nullptr;
-
+	//FBXマネージャー
 	FbxManager* fbxManager = nullptr;
-
+	//FBXインポータ
 	FbxImporter* fbxImporter = nullptr;
-
-private:
-
-	using string = std::string;
-
-public:
-
-	static const string baseDirectory;
-
-	void LoadModelFromFile(const string& modelName);
 
 };
