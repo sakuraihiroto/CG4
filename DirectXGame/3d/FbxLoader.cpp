bcd7289e611492691b3af0e92cbc72a0ef35a639
hirotosakurai <k021g1130@g.neec.ac.jp>
2023-05-11 10:13:52 +0900

    01-08

--- DirectXGame/3d/FbxLoader.cpp
+++ DirectXGame/3d/FbxLoader.cpp
@@ -13,6 +13,9 @@ FbxLoader* FbxLoader::GetInstance()
 const std::string FbxLoader::baseDirectory =
 "Resources/";
 
+const std::string FbxLoader::defaultTextureFileName =
+"white1x1.png";
+
 void FbxLoader::Initialize(ID3D12Device* device)
 {
     //再初期化チェック
@@ -118,9 +121,219 @@ void FbxLoader::ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent)
     }
 
     //FBXノードのメッシュ情報を解析(Todo)
+    FbxNodeAttribute* fbxNodeAttribute = fbxNode->GetNodeAttribute();
+
+    if (fbxNodeAttribute)
+    {
+        if (fbxNodeAttribute->GetAttributeType() == FbxNodeAttribute::eMesh)
+        {
+            model->meshNode = &node;
+            ParseMesh(model, fbxNode);
+        }
+    }
 
     //子ノードに対して再帰呼び出し
     for (int i = 0; i < fbxNode->GetChildCount(); i++) {
         ParseNodeRecursive(model, fbxNode->GetChild(i), &node);
     }
+}
+
+void FbxLoader::ParseMesh(Model* model, FbxNode* fbxNode)
+{
+    //ノードのメッシュを取得
+    FbxMesh* fbxMesh = fbxNode->GetMesh();
+
+    //頂点座標読み取り
+    ParseMeshVertices(model, fbxMesh);
+    //面を構成するデータの読み取り
+    ParseMeshFaces(model, fbxMesh);
+    //マテリアルの読み取り
+    ParseMaterial(model, fbxNode);
+}
+
+void FbxLoader::ParseMeshVertices(Model* model, FbxMesh* fbxMesh)
+{
+    auto& vertices = model->vertices;
+
+    //頂点座標データの数
+    const int controlPointsCount = fbxMesh->GetControlPointsCount();
+    //必要数だけ頂点データ配列を確保
+    Model::VertexPosNormalUv vert{};
+    model->vertices.resize(controlPointsCount, vert);
+
+    //FBX メッシュの頂点座標配列を取得
+    FbxVector4* pCoord = fbxMesh->GetControlPoints();
+}
+
+void FbxLoader::ParseMeshFaces(Model* model, FbxMesh* fbxMesh)
+{
+    auto& vertices = model->vertices;
+    auto& indices = model->indices;
+    
+    //1ファイルに複数メッシュのモデルは非対応
+    assert(indices.size() == 0);
+    //面の数
+    const int polygonCount = fbxMesh->GetPolygonCount();
+    //UVデータの数
+    const int textureUVCount = fbxMesh->GetTextureUVCount();
+    //UV名リスト
+    FbxStringList uvNames;
+    fbxMesh->GetUVSetNames(uvNames);
+
+    //面ごとの情報読み取り
+    for (int i = 0; i < polygonCount; i++)
+    {
+        //面を構成する頂点の数を取得
+        const int polygonSize = fbxMesh->GetPolygonSize(i);
+        assert(polygonSize <= 4);
+
+        //1頂点ずつ処理
+        for (int j = 0; j < polygonSize; j++)
+        {
+            //FBX頂点配列のインデックス
+            int index = fbxMesh->GetPolygonVertex(i, j);
+            assert(index >= 0);
+
+            //頂点法線読込
+            Model::VertexPosNormalUv& vertex = vertices[index];
+            FbxVector4 normal;
+
+            if (fbxMesh->GetPolygonVertexNormal(i, j, normal))
+            {
+                vertex.normal.x = (float)normal[0];
+                vertex.normal.y = (float)normal[1];
+                vertex.normal.z = (float)normal[2];
+            }
+
+            //テクスチャUV読み込み
+            if (textureUVCount > 0)
+            {
+                FbxVector2 uvs;
+                bool lUnmappedUV;
+
+                //0番決め打ちで読み込み
+                if (fbxMesh->GetPolygonVertexUV(i, j, uvNames[0], uvs, lUnmappedUV))
+                {
+                    vertex.uv.x = (float)uvs[0];
+                    vertex.uv.y = (float)uvs[1];
+                }
+            }
+            //インデックス配列に頂点インデックス追加
+            //3頂点目までなら
+            if (j < 3)
+            {
+                //1点追加し、他の2点と三角形を構築する
+                indices.push_back(index);
+            }
+            //4頂点目
+            else
+            {
+                //3点追加
+                //四角形に0,1,2,3の内 2,3,0で三角形を構築する
+                int index2 = indices[indices.size() - 1];
+                int index3 = index;
+                int index0 = indices[indices.size() - 3];
+
+                indices.push_back(index2);
+                indices.push_back(index3);
+                indices.push_back(index0);
+
+            }
+        }
+    }
+}
+
+void FbxLoader::ParseMaterial(Model* model, FbxNode* fbxNode)
+{
+    const int materialCount = fbxNode->GetMaterialCount();
+    if (materialCount > 0)
+    {
+        //先頭のマテリアルを取得
+        FbxSurfaceMaterial* material = fbxNode->GetMaterial(0);
+        //テクスチャを読み込んだかどうかを表すグラフ
+        bool textureLoaded = false;
+
+        if (material)
+        {
+            //FbxSurfaceLambertクラスかどうかを調べる
+            if (material->GetClassId().Is(FbxSurfaceLambert::ClassId))
+            {
+                FbxSurfaceLambert* lambert =
+                    static_cast<FbxSurfaceLambert*>(material);
+
+                //環境光係数
+                FbxPropertyT<FbxDouble3>ambient = lambert->Ambient;
+                model->ambient.x = (float)ambient.Get()[0];
+                model->ambient.y = (float)ambient.Get()[1];
+                model->ambient.z = (float)ambient.Get()[2];
+
+                //拡散反射光係数
+                FbxPropertyT<FbxDouble3>diffuse = lambert->Diffuse;
+                model->diffuse.x = (float)diffuse.Get()[0];
+                model->diffuse.y = (float)diffuse.Get()[1];
+                model->diffuse.z = (float)diffuse.Get()[2];
+            }
+            //ディフューズテクスチャを取り出す
+            const FbxProperty diffuseProperty =
+                material->FindProperty(FbxSurfaceMaterial::sDiffuse);
+            if (diffuseProperty.IsValid())
+            {
+                const FbxFileTexture* texture = diffuseProperty.GetSrcObject<FbxFileTexture>();
+                if (texture)
+                {
+                    const char* filepath = texture->GetFileName();
+                    //ファイルパスからファイル名抽出
+                    string path_str(filepath);
+                    string name = ExtractFileName(path_str);
+                    //テクスチャ読み込み
+                    LoadTexture(model, baseDirectory + model->name + "/" + name);
+                    textureLoaded = true;
+                }
+            }
+        }
+        //テクスチャがない場合は白テクスチャを貼る
+        if (!textureLoaded)
+        {
+            LoadTexture(model,
+                baseDirectory + defaultTextureFileName);
+        }
+    }
+}
+
+void FbxLoader::LoadTexture(Model* model, const std::string& fullpath)
+{
+    HRESULT result = S_FALSE;
+    //WICテクスチャのロード
+    TexMetadata& metadata = model->metadata;
+    ScratchImage& scratchImg = model->scratchImg;
+    //ユニコード文字列に変換
+    wchar_t wfilepath[128];
+    MultiByteToWideChar(CP_ACP, 0, fullpath.c_str(), -1, wfilepath,
+        _countof(wfilepath));
+    result = LoadFromWICFile(
+        wfilepath, WIC_FLAGS_NONE,
+        &metadata, scratchImg);
+    if (FAILED(result))
+    {
+        assert(0);
+    }
+}
+
+std::string FbxLoader::ExtractFileName(const std::string& path)
+{
+    size_t pos1;
+    //区切り文字'\\'が出てくる一番最後の部分を検索
+    pos1 = path.rfind('\\');
+    if (pos1 != string::npos)
+    {
+        return path.substr(pos1 + 1, path.size() - pos1 - 1);
+    }
+    //区切り文字'/'が出てくる一番最後の部分を検索
+    pos1 = path.rfind('/');
+    if (pos1 != string::npos)
+    {
+        return path.substr(pos1 + 1, path.size() - pos1 - 1);
+    }
+
+    return path;
 }
--- DirectXGame/3d/Model.h
+++ DirectXGame/3d/Model.h
@@ -2,6 +2,11 @@
 #include<string>
 #include<DirectXMath.h>
 #include<vector>
+#include<DirectXTex.h>
+#include<Windows.h>
+#include<wrl.h>
+#include<d3d12.h>
+#include<d3dx12.h>
 
 struct Node
 {
@@ -26,9 +31,49 @@ class Model
 public:
 	//フレンドクラス
 	friend class FbxLoader;
+public://サブクラス
+	//頂点データ構造体
+	struct VertexPosNormalUv
+	{
+		DirectX::XMFLOAT3 pos;//xyz座標
+		DirectX::XMFLOAT3 normal;//法線ベクトル
+		DirectX::XMFLOAT2 uv;//uv座標
+	};
 private:
 	//モデル名
 	std::string name;
 	//ノード配列
 	std::vector<Node> nodes;
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv>vertices;
+	//頂点インデックス配列
+	std::vector<unsigned short>indices;
+	//アンビエント係数
+	DirectX::XMFLOAT3 ambient = { 1,1,1 };
+	//ディフューズ係数
+	DirectX::XMFLOAT3 diffuse = { 1,1,1 };
+	//テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	//スクラッチイメージ
+	DirectX::ScratchImage scratchImg = {};
+
+private: //エイリアンス
+
+	template<class  T>using Comptr = Microsoft::WRL::ComPtr<T>;
+	//DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+	using TexMetadata = DirectX::TexMetadata;
+	using ScratchImage = DirectX::ScratchImage;
+
+	using string = std::string;
+	template<class T>using vector = std::vector<T>;
+	
+
+	
+
 };
